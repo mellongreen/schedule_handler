482a735960bc7a9892bb2aef93b907ba055b968d
Suphavit Pattanapitoon <suphavitp@hotmail.com>
2015-08-18 01:11:04 -0700

    license added

--- schedule_handler.ino
+++ schedule_handler.ino
@@ -1,4 +1,23 @@
 /*
+schedule handler for scheduling power control
+Copyright (C) 2015 Mellon Green
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
+/*
   The circuit:
  * LCD RS pin to digital pin 12
  * LCD Enable pin to digital pin 11
